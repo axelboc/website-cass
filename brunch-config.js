9c3f4763747060e9a25e21bb0150250c1494ed0c
Axel Bocciarelli <axelboc@gmail.com>
2016-08-02 22:17:13 +1000

    convert brunch config to JS

--- brunch-config.js
+++ brunch-config.js
@@ -1,11 +1,15 @@
-module.exports = config:
-	files:
-		stylesheets: joinTo: 'main.css'
-	plugins:
-		postcss:
+module.exports = {
+	files: {
+		stylesheets: { joinTo: 'main.css' }
+	},
+	plugins: {
+		postcss: {
 			processors: [
 				require('postcss-import')({ path: 'app/css' }),
 				require('postcss-nested'),
 				require('postcss-custom-properties'),
 				require('cssnano')({ autoprefixer: ['> 2% in AU'] })
 			]
+		}
+	}
+};
