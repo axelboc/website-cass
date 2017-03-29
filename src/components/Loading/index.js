90e2ca023b5d360df5b3066a1c0167cba3d689bf
Axel Bocciarelli <axelboc@gmail.com>
2017-03-29 21:50:34 +1100

    show top-bar progress indicator

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import styles from './index.css';
+import Helmet from "react-helmet";
+import ProgressIndicator from "react-topbar-progress-indicator";
 import Spinner from '../Spinner';
+import styles from './index.css';
+
+ProgressIndicator.config({
+  barThickness: 2,
+  shadowBlur: 8,
+  shadowColor: "rgba(32, 32, 48, 0.5)",
+  barColors: {
+    "0": "#af3636",
+  }
+});
 
 const Loading = () => (
   <div className={styles.loading}>
+    <Helmet title="Loading..." />
+    <ProgressIndicator />
     <Spinner />
   </div>
 );
