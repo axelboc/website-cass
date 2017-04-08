224f65b9b82ac4ccf1f8d1a52848237a813e5545
Axel Bocciarelli <axelboc@gmail.com>
2017-04-08 17:38:02 +1000

    tweak FourImages layout

--- collections/works/thin-places.md
+++ collections/works/thin-places.md
@@ -12,5 +12,3 @@ assets:
 A collection of short graphic narratives about traveling to Thin Places in the world and the strange things that happen there.
 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam deleniti hic obcaecati nesciunt impedit corporis soluta non laudantium provident dolor quidem illo, quae asperiores reprehenderit perferendis doloribus officia suscipit tenetur!
-
-Fuga illo pariatur nemo impedit quidem mollitia quos repudiandae aut, in, exercitationem ab eos! Ipsa, architecto accusamus ullam quos incidunt, necessitatibus eos sit, eveniet numquam excepturi reprehenderit quo animi dolorum.
--- src/components/Work/FourImages/index.js
+++ src/components/Work/FourImages/index.js
@@ -6,15 +6,15 @@ import styles from "./index.css";
 const FourImages = ({ images, body }) => (
   <div className={styles.root}>
     <div className={styles.rowOne}>
+      <div className={styles.body}>{body && renderHTML(body)}</div>
+      <img className={styles.bodyImage} src={`/assets/work/${images[3]}`} />
+    </div>
+    <div className={styles.rowTwo}>
       <img className={styles.mainImage} src={`/assets/work/${images[0]}`} />
       <div className={styles.sideImages}>
         <div style={{backgroundImage: `url(/assets/work/${images[1]})`}} />
         <div style={{backgroundImage: `url(/assets/work/${images[2]})`}} />
       </div>
-    </div>
-    <div className={styles.rowTwo}>
-      <div className={styles.body}>{body && renderHTML(body)}</div>
-      <img className={styles.lastImage} src={`/assets/work/${images[3]}`} />
     </div>
   </div>
 );
