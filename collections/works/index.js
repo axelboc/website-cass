897cc8e908b81a09a8e9e7355b84e22c0fd3a171
Axel Bocciarelli <axelboc@gmail.com>
2017-10-04 20:47:36 +1100

    add Djerassi work item

--- collections/works/index.js
+++ collections/works/index.js
@@ -1,5 +1,6 @@
 import antiThesis from "./anti-thesis.md";
 import blog from "./blog.md";
+import djerassi from "./djerassi.md";
 import glenfern from "./glenfern.md";
 import morrigan from "./morrigan.md";
 import northernLights from "./northern-lights.md";
@@ -7,6 +8,7 @@ import thinPlaces from "./thin-places.md";
 
 // Export items in specific order
 const works = [
+  djerassi,
   glenfern,
   blog,
   northernLights,
